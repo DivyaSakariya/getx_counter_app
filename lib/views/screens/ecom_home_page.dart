c495d2b0133791571ead22806290e75de9b27709
yogesh1825 <kumavatyogesh92@gmail.com>
2023-08-18 11:47:08 -0400

    DS | 18/08/2023 11:47 # Ecommerce App

---
+++ lib/views/screens/ecom_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EcomHomePage extends StatelessWidget {
+  const EcomHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
